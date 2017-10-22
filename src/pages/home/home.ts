ed23a19d72e7550b2df5c760857c6be7a4a0d1dc
adrianmarello <adrianmarello@gmail.com>
2017-10-21 22:49:55 -0300

    home con menu e iconos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 import { CategoriesPage } from '../categories/categories';
 import { MapaPage } from '../mapa/mapa';
@@ -14,18 +15,29 @@ import { SobreCatamarcaPage } from '../sobre-catamarca/sobre-catamarca';
 export class HomePage {
 
     items: any;
+    @ViewChild(Slides) slides: Slides;
 
     constructor(public navCtrl: NavController) {
         this.items = [
-            { nombre: 'mapa', component: MapaPage },
-            { nombre: 'atractivos', component: CategoriesPage },
-            { nombre: 'busqueda', component: BusquedaPage },
-            { nombre: 'catamarca', component: SobreCatamarcaPage },
+            { titulo:'mapa', subtitulo: 'Encontra lo que buscas cerca de tí', color: '#3276b1', icon:'assets/images/icono-mapa.png', component:MapaPage },
+            { titulo:'atractivos', subtitulo: 'Descubrí Catamarca', color: '#5cb85c', icon:'assets/images/icono-turista.png', component:CategoriesPage},
+            { titulo:'busqueda', subtitulo: 'Que estas buscando ?', color: '#f0ad4e', icon:'assets/images/icono-buscar.png', component:BusquedaPage },
+            { titulo:'catamarca', subtitulo: 'Acerca de Catamarca', color: '#d9534f', icon:'assets/images/icono-historia.png', component:SobreCatamarcaPage },
         ]
     }
 
     itemSelected(item: any) {
         this.navCtrl.push(item.component);
+    }
+
+    changeBackground(img: string): any {
+        console.log(img);
+        return { 'background': 'url('+ img +') center no-repeat'};
+    }
+
+    slideChanged() {
+        console.log('changed slide');
+        this.slides.startAutoplay();
     }
 
 }
