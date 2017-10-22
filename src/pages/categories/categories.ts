27419234c99d38f96c4ba6bb915234410d13d42f
adrianmarello <adrianmarello@gmail.com>
2017-10-22 16:23:38 -0300

    se agrego lodash al package.json - se dio un estilo a la lista de categorias y lista de pois

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import * as _ from "lodash";
+
 import { SgturPOIProvider } from '../../providers/sgtur/poi';
 import { PoiListPage } from '../poi-list/poi-list';
 
