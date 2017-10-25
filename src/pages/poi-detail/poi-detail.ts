67479c1ccedd26892cbe84d224a099692d4bca79
Martin Miranda <debianitram@gmail.com>
2017-10-25 17:58:49 -0300

    Se agregó el componente de servicios al componente detalle de poi, se generó el providers para hospedajes

--- src/pages/poi-detail/poi-detail.ts
+++ src/pages/poi-detail/poi-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 
 import { CoreProvider } from './../../providers/core/core'
 import { PoiDetailModalDescPage } from './../poi-detail-modal-desc/poi-detail-modal-desc';
+import { PoiDetailServiciosPage } from './../poi-detail-servicios/poi-detail-servicios';
 
 
 @IonicPage()
@@ -31,6 +32,10 @@ export class PoiDetailPage {
         let modal = null;
         if (type == "detalle"){
             modal = this.modalCtrl.create(PoiDetailModalDescPage, { titulo: data.titulo, detalle: data.descripcion });
+        }
+
+        if (type == "servicios"){
+            modal = this.modalCtrl.create(PoiDetailServiciosPage, {'data': data});
         }
         modal.present();
     }
